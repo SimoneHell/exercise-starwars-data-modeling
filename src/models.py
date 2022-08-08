2bb5bcde5e75e97132b8ea0e3466d69106eee8c9
SimoneHell <simone.hall.speaks@gmail.com>
2022-08-08 18:53:32 +0000

    Adding tables

--- src/models.py
+++ src/models.py
@@ -15,6 +15,24 @@ class Person(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    password = Column (String(250), nullable=False)
+    favorite_id = (Integer, primary_key=True, ForeignKey ('Favorite.id'))
+
+#Favourite 
+-
+id int pk 
+UserId int pk fk >- User.UserID
+class Favorite(Base):
+   __tablename__="favorite"
+   id = Column(Integer, primary_key=True) 
+   UserId = Column(Integer, primary_key, ForeignKey ('User.UserID'))
+   
 class Address(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
